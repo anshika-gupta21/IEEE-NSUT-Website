6308edd7fcb05082a4ec70ad115a73134173f85e
utkrsh1203 <utkrsh2212@gmail.com>
2023-10-03 03:42:42 +0530

    Team section done

--- IEEE_2023_Website/src/components/AwardsnAchievements/Acheive.jsx
+++ IEEE_2023_Website/src/components/AwardsnAchievements/Acheive.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import "./Awards.css"
-import Awardcar from './Awardcar'
+import React from "react";
+import "./Awards.css";
+import Awardcar from "./Awardcar";
 function Awards() {
   return (
-    <div className='awards'>
-        <div className='bold_wrap'>
-
-        <div className='award_1'>Our Achievements</div>
+    <div className='awards my-20' id='achievements'>
+      <div className='bold_wrap'>
+        <div className='award_1'>ACHIEVEMENTS</div>
         <div className='award_2'>Awards</div>
-        </div>
-        <Awardcar/>
+      </div>
+      <Awardcar />
     </div>
-  )
+  );
 }
 
-export default Awards
+export default Awards;
--- IEEE_2023_Website/src/components/events/events.jsx
+++ IEEE_2023_Website/src/components/events/events.jsx
@@ -1,93 +1,103 @@
-import React from 'react'
-import './Events.css'
-import {MdSchedule} from 'react-icons/md'
+import React from "react";
+import "./Events.css";
+import { MdSchedule } from "react-icons/md";
 function Events() {
-    let [over1, setOver1] = React.useState(false);
-    let [over2, setOver2] = React.useState(false);
-    let buttonstyle1 = {
-        textDecoration: 'none',
-        color: 'white'
-    }
-    let buttonstyle2 = {
-        textDecoration: 'none',
-        color: 'white'
-    }
-    if(over1) {
-        buttonstyle1.color = '#1B72F7'
-    } else if(over2) {
-        buttonstyle2.color = '#1B72F7'
-    } else if(!over1) {
-        buttonstyle1.color = 'white'
-    } else if(!over2) {
-        buttonstyle2.color = 'white'
-    }
+  let [over1, setOver1] = React.useState(false);
+  let [over2, setOver2] = React.useState(false);
+  let buttonstyle1 = {
+    textDecoration: "none",
+    color: "white",
+  };
+  let buttonstyle2 = {
+    textDecoration: "none",
+    color: "white",
+  };
+  if (over1) {
+    buttonstyle1.color = "#1B72F7";
+  } else if (over2) {
+    buttonstyle2.color = "#1B72F7";
+  } else if (!over1) {
+    buttonstyle1.color = "white";
+  } else if (!over2) {
+    buttonstyle2.color = "white";
+  }
   return (
-    <div className='EventPage' id="events">
-    <div className="event">EVENTS</div>
-    <div className="card-header">Explore Future Events</div>
-    <div className="card-container">
-
-          <div className="card">
-              <div className="date-container">
-                  <div className="date">3</div>
-                  <div className="month-year">
-                      <div className="month">October</div>
-                      <div className="year">2023</div>
-                  </div>
-              </div>
-
-              <div className="timing">
-                  <MdSchedule/>
-                  <p>12:00 PM</p>
-              </div>
-              <div className="divider-line"></div>
-
-              <div className="card-content">
-                  <h3>IEEE-Day 2023</h3>
-                  <div className="description">Session by Evenidk
-                  </div>
-              </div>
-
-              <button className="read-more" onMouseOver={()=>setOver1(true)} onMouseOut={()=>setOver1(false)}>
-              <a href="https://www.instagram.com/p/CjM7Et2uEea/" style={buttonstyle1}>Explore Now</a>
-                </button>
-
-          </div>
-         
-          <div className="card">
-              <div className="date-container">
-                  <div className="date g-1">13</div>
-                  <div className="month-year g-2">
-                      <div className="month">October</div>
-                      <div className="year">2023</div>
-                  </div>
-              </div>
-
-              <div className="timing">
-              <MdSchedule/>
-                  <p>12:00 PM </p>
-              </div>
-
-              <div className="divider-line"></div>
-
-              <div className="card-content">
-                  <h3>DSC Delhi Section</h3>
-                  <div className="description">An ideathon under project Inspire
-                  </div>
-              </div>
-
-              <button className="read-more" onMouseOver={()=>setOver2(true)} onMouseOut={()=>setOver2(false)}>
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLScG4TdIlJONHAQ0OVAnuDb0Y9wHpibsXadd9BXd6boaQGhx_w/viewform" style={buttonstyle2}> Explore Now</a>
-                </button>
+    <div className='EventPage' id='events'>
+      <div className='event'>EVENTS</div>
+      <div className='card-header'>Explore Future Events</div>
+      <div className='card-container'>
+        <div className='card'>
+          <div className='date-container'>
+            <div className='date'>3</div>
+            <div className='month-year'>
+              <div className='month'>October</div>
+              <div className='year'>2023</div>
+            </div>
           </div>
 
+          <div className='timing'>
+            <MdSchedule />
+            <p>12:00 PM</p>
+          </div>
+          <div className='divider-line'></div>
 
+          <div className='card-content'>
+            <h3>IEEE-Day 2023</h3>
+            <div className='description'>Session by Evenidk</div>
+          </div>
 
-         
+          <button
+            className='read-more'
+            onMouseOver={() => setOver1(true)}
+            onMouseOut={() => setOver1(false)}
+          >
+            <a
+              href='https://www.instagram.com/p/CjM7Et2uEea/'
+              style={buttonstyle1}
+            >
+              Explore Now
+            </a>
+          </button>
+        </div>
 
+        <div className='card'>
+          <div className='date-container'>
+            <div className='date g-1'>13</div>
+            <div className='month-year g-2'>
+              <div className='month'>October</div>
+              <div className='year'>2023</div>
+            </div>
+          </div>
+
+          <div className='timing'>
+            <MdSchedule />
+            <p>12:00 PM </p>
+          </div>
+
+          <div className='divider-line'></div>
+
+          <div className='card-content'>
+            <h3>DSC Delhi Section</h3>
+            <div className='description'>An ideathon under project Inspire</div>
+          </div>
+
+          <button
+            className='read-more'
+            onMouseOver={() => setOver2(true)}
+            onMouseOut={() => setOver2(false)}
+          >
+            <a
+              href='https://docs.google.com/forms/d/e/1FAIpQLScG4TdIlJONHAQ0OVAnuDb0Y9wHpibsXadd9BXd6boaQGhx_w/viewform'
+              style={buttonstyle2}
+            >
+              {" "}
+              Explore Now
+            </a>
+          </button>
+        </div>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default Events
+export default Events;
